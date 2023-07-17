bfdeff52d6e2fe7c24c3cc3223b63628a9c59ca6
BuildTools <unconfigured@null.spigotmc.org>
2023-07-17 12:23:40 +0200

    Added registration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Add from '../img/addAvatar.png'
 
 function Register() {
     return (
-        <div>Register</div>
+        <div className='formContainer'>
+            <div className='formWrapper'>
+                <form>
+                    <span className='logo'>React Chat</span>
+                    <span className='title'>Register</span>
+                    <input type="text" placeholder='display name' />
+                    <input type="email" placeholder='email' />
+                    <input type="password" placeholder='password' />
+                    <input type="file" id='file' style={{ display: 'none' }} />
+                    <label htmlFor="file">
+                        <img src={Add} alt="" />
+                        <span>Add an avatar</span>
+                    </label>
+                    <button>Sign up</button>
+                    <p>Already have an account? Login</p>
+                </form>
+            </div>
+        </div>
     )
 }
 
