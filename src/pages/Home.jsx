dc84c4b359307a34a70e0b0d7906608c6c0bb230
BuildTools <unconfigured@null.spigotmc.org>
2023-07-17 13:07:56 +0200

    Added basic homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className="home">
+            <div className="container">
+                <Sidebar />
+                <Chat />
+            </div>
+        </div>
     )
 }
 
