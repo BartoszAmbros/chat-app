9df564528436c1ee48b7d204a2e6268952dca17c
BuildTools <unconfigured@null.spigotmc.org>
2023-08-08 14:06:15 +0200

    Added basic firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAaauUuXNxzcHcM6lJdy7Ik_fkMjdQafPY",
+    authDomain: "chat-app-ba6b1.firebaseapp.com",
+    projectId: "chat-app-ba6b1",
+    storageBucket: "chat-app-ba6b1.appspot.com",
+    messagingSenderId: "533416060980",
+    appId: "1:533416060980:web:ddc1c62681a8f21e928261"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
