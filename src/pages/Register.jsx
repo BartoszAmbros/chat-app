2a1c34d6d42bac98038e11ee5b8dcf517014d150
BuildTools <unconfigured@null.spigotmc.org>
2023-08-08 14:27:00 +0200

    Added basic registration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Add from '../img/addAvatar.png'
+import { auth } from '../firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react'
 
 function Register() {
+    const [err, setErr] = useState(false);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].value;
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+            setErr(true)
+        }
+    }
     return (
         <div className='formContainer'>
             <div className='formWrapper'>
                 <span className='logo'>React Chat</span>
                 <span className='title'>Register</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder='display name' />
                     <input type="email" placeholder='email' />
                     <input type="password" placeholder='password' />
@@ -17,6 +36,7 @@ function Register() {
                         <span>Add an avatar</span>
                     </label>
                     <button>Sign up</button>
+                    {err && <span>Something went wrong</span>}
                 </form>
                 <p>Already have an account? Login</p>
             </div>
